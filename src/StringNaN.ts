134016cac53bb30e9032d261a89ee08ac3503a9e
MegPunyu <meg.punyu@meg.punyu.jp>
2023-07-28 13:04:25 +0900

    version 3.0.0

--- src/StringNaN.ts
+++ src/StringNaN.ts
@@ -1,11 +1,6 @@
+import QNanEnumerator from "qnan-enumerator";
+
 export default class StringNaN {
-
-    // 8 bytes buffer (stores a double precision floating point number)
-    private static readonly buf: DataView = new DataView(new ArrayBuffer(8));
-
-    static {
-        this.buf.setUint16(0, 0xFFF8, false);  // bit pattern for quiet NaN (IEEE 754)
-    }
 
     /**
      * Converts a string to a NaN array.
@@ -15,33 +10,24 @@ export default class StringNaN {
      * StringNaN.toNaNs("meg");  // [ NaN, NaN, NaN ]
      */
     public static toNaNs(str: string): typeof NaN[] {
-        return str.split("").map(char => {
-            this.buf.setUint32(2, char.charCodeAt(0) + 1, false);
-            return this.buf.getFloat64(0, false);
-        });
+        const qne = new QNanEnumerator();
+
+        return str.split("").map(chr => qne.getNan(chr.charCodeAt(0)));
     }
 
     /**
      * Converts a NaN array to a string.
      * 
      * @param nans a NaN or a NaN Array to convert to a string
-     * @throws {TypeError} if nans is not a NaN or a NaN Array
      * @example
      * const meg = StringNaN.toNaNs("meg");
      * StringNaN.toString(meg.reverse());  // "gem"
      */
     public static toString(nans: typeof NaN | typeof NaN[]): string {
+        const qne = new QNanEnumerator();
 
-        const charCodes: number[] = (typeof nans === "number" ? [nans] : nans).map(nan => {
-
-            if (!Number.isNaN(nan)) {
-                throw new TypeError("Non NaN values found");
-            }
-
-            this.buf.setFloat64(0, nan, false);
-
-            return this.buf.getUint32(2, false) - 1;
-        });
+        const charCodes: number[] = (typeof nans === "number" ? [nans] : nans)
+            .map(nan => qne.getId(nan));
 
         return String.fromCharCode(...charCodes);
     }
